5933274d61fc7ecf7e3d2ab54a6cb388a7382226
Alexandre Junior Miranda <alejunior@msn.com>
2021-02-09 10:37:53 -0300

    formulario reativo finalizado

--- src/app/tasks/task-detail/task-datail.component.ts
+++ src/app/tasks/task-detail/task-datail.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Task } from "../shared/task.model";
 import { TaskService } from "../shared/task.service";
@@ -11,42 +12,85 @@ import { TaskService } from "../shared/task.service";
 })
 export class TaskDetailComponent implements OnInit, AfterViewInit {
   
-  public task: Task = {id: null, title: null};
-
-  public taskDoneOptions: Array<any> = [
-    { value: false, text: "Pendente" },
-    { value: true, text: "Pronto" }
-  ]
+  public task: Task;
+  
+  public taskDoneOptions: Array<any>;
+  
+  public reactiveTaskForm: FormGroup;
   
   public constructor(
     private taskService: TaskService,
     private route: ActivatedRoute,
-    private location: Location
-  ) { }
+    private location: Location,
+    private formBuilder: FormBuilder
+  ) { 
+    this.task = new Task(null, null);
 
-  public ngAfterViewInit(): void {
-    // aguarda o templete ser carregado para iniciar  que estiver aqui.
+    this.taskDoneOptions = [
+      { value: false, text: "Pendente" },
+      { value: true, text: "Pronto" }
+    ];
+
+    this.reactiveTaskForm = this.formBuilder.group({
+      title: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(50)]],
+      deadline: [null, Validators.required],
+      done: [null, Validators.required],
+      description: [null],
+      user: this.formBuilder.group({
+        name: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(50)]],
+        email: [null, [Validators.required, Validators.email]]
+      })
+    });
   }
-
+  
   public ngOnInit(): void {
     let id: number;
     this.route.params.subscribe((params) => id = params['id']); //pegar o :id da rota
     this.taskService.getTask(id)
-      .subscribe(
-        task => this.task = task,
-        err => { alert("servidor indisponível, tente mais tarde."), console.log(err) },
+    .subscribe(
+      task => { this.task.id = task.id, this.setTaskForm(task) },
+      err => { alert("servidor indisponível, tente mais tarde"), console.log(err) },
       )
     }
 
+  public setTaskForm(task:Task): void {
+    this.reactiveTaskForm.setValue({
+      title: task.title || null,
+      deadline: task.deadline || null,
+      done: task.done || null,
+      description: task.description || null,
+      user: { name: "joao carlos", email: "joao@carlos.com"}
+    });
+
+    //  //patchValue usado quando não passa todos os campos
+    // this.reactiveTaskForm.patchValue({
+    //   title: task.title || null,
+    //   deadline: task.deadline || null
+    // });
+  }
+
+  public ngAfterViewInit(): void {
+    // aguarda o templete ser carregado para iniciar  que estiver aqui.
+  }
+    
   public goBack() {
     this.location.back();
   }
 
-  public updateTask() {
-    this.taskService.updateTask(this.task)
+  public updateTask(task:Task) {
+    task.id = this.task.id;
+    this.taskService.updateTask(task)
       .subscribe(
         () => { alert("Salvo com sucesso.") },
         err => { alert("servidor indisponível, tente mais tarde."), console.log(err) },
       )
+  }
+
+  // validação do template
+  public fieldClassError(fieldName: string) {
+    return {
+      "is-invalid": this.reactiveTaskForm.get(fieldName).invalid && (this.reactiveTaskForm.get(fieldName).touched || this.reactiveTaskForm.get(fieldName).dirty),
+      "is-valid": this.reactiveTaskForm.get(fieldName).valid && (this.reactiveTaskForm.get(fieldName).touched || this.reactiveTaskForm.get(fieldName).dirty)
+    }
   }
 }
